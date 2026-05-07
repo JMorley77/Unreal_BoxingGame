51711be017cf1fbf5e41152df8b19ab149885bc5
DESKTOP-OG6GRRU\James <James.Morley77@outlook.com>
2026-05-07 18:52:43 +0100

    Health working for both AI and Player

--- AI_Project/enc_temp_folder/e3732e3ec92e253464b9da59de06dbd/Character1.h
+++ AI_Project/enc_temp_folder/e3732e3ec92e253464b9da59de06dbd/Character1.h
@@ -28,7 +28,6 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat")
 	int health = 100;
 	int damage = 20;
 
